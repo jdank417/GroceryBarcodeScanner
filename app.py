1dd1059a05fdc723a4ded0ece6eeb99b1e4bf62c
Aliya Vagapova <vagapovaa@wit.edu>
2025-02-04 21:40:01 -0500

    about section added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, flash
-import pandas as pd
-import os
 from functools import lru_cache
+
+import pandas as pd
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
